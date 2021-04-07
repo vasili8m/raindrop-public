475665c4bedbbb7d1054d6e48ef2f1c7880aa827
Rustem Mussabekov <winlinx@ya.ru>
2021-04-07 11:31:45 +0300

    test subdoimain again

--- src/pages/api/map-sub-domain.js
+++ src/pages/api/map-sub-domain.js
@@ -1,10 +1,13 @@
 import http from 'http'
+import https from 'https'
 
 export default function handler(req, res) {
+    const isDev = process.env.NODE_ENV!='production'
+
     const { host } = req.headers
     const name = host.split('.')[0]
+    const destination = new URL(`/user/${name}`, `${isDev?'http':'https'}://${host}`)
 
-    const destination = new URL(`/user/${name}`, `https://${host}`)
-
-    http.get(destination, (r)=>r.pipe(res))
+    const how = isDev ? http : https
+    how.get(destination, (r)=>r.pipe(res))
 }
