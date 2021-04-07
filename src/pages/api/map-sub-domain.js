185c3d96c730a10f385c374ea8a8f65e20a478b8
Rustem Mussabekov <winlinx@ya.ru>
2021-04-07 11:28:01 +0300

    test again

--- src/pages/api/map-sub-domain.js
+++ src/pages/api/map-sub-domain.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
     const { host } = req.headers
     const name = host.split('.')[0]
 
-    const destination = new URL(`/user/${name}`, `http://${host}`)
+    const destination = new URL(`/user/${name}`, `https://${host}`)
 
     http.get(destination, (r)=>r.pipe(res))
 }
