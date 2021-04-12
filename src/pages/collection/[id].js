5a091b2d02ebe0e8bf5f548cff62f177b291aa43
Rustem Mussabekov <winlinx@ya.ru>
2021-04-12 16:44:18 +0300

    Redirect old routes

---
+++ src/pages/collection/[id].js
@@ -0,0 +1,31 @@
+import Api from '~api'
+
+const cache = {}
+
+async function getUrl(id) {
+    if (cache[id]) return cache[id]
+
+    const collection = await Api.collection.get(id)
+    if (!collection) return null
+
+    const user = await Api.user.get(collection.user?.$id)
+    if (!user) return null
+
+    return cache[id]=`https://raindrop.io/${user.name}/${collection.slug}-${collection._id}`
+}
+
+export async function getServerSideProps({ params: { id }, res }) {
+    const url = await getUrl(id)
+    if (!url)
+        return { notFound: true }
+
+    res.statusCode = 308;
+    res.setHeader('Location', url)
+    res.end()
+
+    return { props: {} }
+}
+
+export default function Oembed() {
+    return null
+}
---
+++ src/pages/user/[id].js
@@ -0,0 +1,28 @@
+import Api from '~api'
+
+const cache = {}
+
+async function getUrl(id) {
+    if (cache[id]) return cache[id]
+
+    const user = await Api.user.get(id)
+    if (!user) return cache[id]=null
+
+    return cache[id]=`https://raindrop.io/${user.name}`
+}
+
+export async function getServerSideProps({ params: { id }, res }) {
+    const url = await getUrl(id)
+    if (!url)
+        return { notFound: true }
+
+    res.statusCode = 308;
+    res.setHeader('Location', url)
+    res.end()
+
+    return { props: {} }
+}
+
+export default function Oembed() {
+    return null
+}
