f35ed627d5b45fe97149f33557271230f19869c4
Rustem Mussabekov <winlinx@ya.ru>
2021-04-27 15:14:23 +0300

    Fix non-js pages with header

--- src/co/page/subheader/index.js
+++ src/co/page/subheader/index.js
@@ -2,7 +2,7 @@ import s from './index.module.css'
 
 export default function Subheader({ children }) {
     return (
-        <aside className={s.subheader}>
+        <aside className={s.subheader} data-is-subheader>
             {children}
         </aside>
     )
