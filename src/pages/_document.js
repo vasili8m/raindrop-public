80a60062025aa3686ba771868b6baf0ec65d71dc
Rustem Mussabekov <winlinx@ya.ru>
2021-05-02 16:44:34 +0300

    Fix view on a mobile

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,8 @@ class MyDocument extends Document {
         return (
             <Html lang='en'>
                 <Head>
+                    <meta name='viewport' content='width=device-width,height=device-height,initial-scale=1' />
+
                     <meta name='og:site_name' content='Raindrop.io' />
                     <meta name='twitter:site' content='@raindrop_io' />
                     <meta name='twitter:domain' content='raindrop.io' />
